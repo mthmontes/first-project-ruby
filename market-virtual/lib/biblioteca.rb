11e7a37972a02c3b8c3e4b129f1015b2a3f96076
Matheus <plutaozaum@gmail.com>
2023-06-20 22:56:28 -0300

    I don't no what I'm doing

--- market-virtual/lib/biblioteca.rb
+++ market-virtual/lib/biblioteca.rb
@@ -5,23 +5,29 @@ class Biblioteca
   attr_reader :livros
 
   def initialize
-    @livros = []
+    @livros = {}
   end
 
   def adiciona(livro)
-    @livros << livro
+    @livros[livro.category] ||= []
+    @livros[livro.category] << livro
+  end
+
+  def livros
+    @livros.values.flatten
   end
 
 end
 
 biblioteca = Biblioteca.new
 
-livro_de_python = Book.new "Python", 0101, 400, 80.00
-livro_ruby = Book.new "Ruby", 0202, 300, 79.00
+livro_de_python = Book.new "Python", 0101, 400, 80.00, :testes
+livro_ruby = Book.new "Ruby", 0202, 300, 79.00, :ruby
 
 biblioteca.adiciona livro_de_python
 biblioteca.adiciona livro_ruby
 
-for livro in biblioteca.livros do
-  puts livro.price
+
+for price in biblioteca.livros do
+  puts price
 end
--- market-virtual/lib/book.rb
+++ market-virtual/lib/book.rb
@@ -3,12 +3,14 @@
 class Book
 
     attr_accessor :price
+    attr_reader :category
 
-    def initialize(name, cod_id, pages, price)
+    def initialize(name, cod_id, pages, price, category)
         @name = name
         @cod_id = cod_id
         @pages = pages
         @price = price
+        @category = category
     end
 
     def price
@@ -16,13 +18,16 @@ class Book
     end
 
     def to_s
-        "Name of book: #{@name}, Code: #{@cod_id}, Pages: #{@pages}, Price: #{@price}"
+        "Name of book: #{@name}, Code: #{@cod_id}, Pages: #{@pages}, Price: #{@price}, Category: #{@category}"
     end
 
 end
 
-book_ruby = Book.new("Linguagem Ruby", 1234, 400, 80.00)
-book_python = Book.new("Linguagem Python", 12345, 300, 86.00)
+book_ruby = Book.new("Linguagem Ruby", 1234, 400, 80.00, :ruby)
+book_python = Book.new("Linguagem Python", 0101, 300, 86.00, :python)
 
-puts book_ruby
-puts book_python
+hash = {"1234" => book_ruby,
+        "0101" => book_ruby}
+
+#puts hash["1234"]
+
